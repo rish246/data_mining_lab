87ff479d5475ac9c81581403a08e9428957314da
rish246 <rishabhkatna2228@gmail.com>
2021-03-17 18:25:33 +0530

    refactored the whole script into diff functions

--- Practical5/Cmd_implementation.py
+++ Practical5/Cmd_implementation.py
@@ -1,104 +1,41 @@
-def main():
-    test_matrix1 = [
-    [250, 16, 12, 5],
-    [200, 16, 8, 3],
-    [300, 32, 16, 4],
-    [275, 32, 8, 4],
-    [225, 16, 16, 2]
-    ]
+'''
+Command line implementation of the TOPSIS algorithm
 
-    n_rows = 5
-    n_cols = 4
-
-    weights = [0.25, 0.25, 0.25, 0.25]
-    actions = ['-', '+', '+', '+']
-
-    ##################### Calculate RMS values #########################
+Author: Rishabh Katna
+'''
+def merge_matrix_and_scores(test_matrix1, n_rows, performance_score, ranks):
+    
+    for row in range(n_rows):
+    
+        test_matrix1[row] = test_matrix1[row] + [performance_score[row], ranks[row]]
 
 
-    rms_values_cols = [0 for _ in range(n_cols)]
+def get_ranks(performance_score):
 
-    for col in range(n_cols):
+    performance_score_copy = performance_score.copy()
 
-        for row in range(n_rows):
+    performance_score_copy.sort(reverse=True)
 
-            rms_values_cols[col] += (test_matrix1[row][col] ** 2)
+    score_to_rank = {}
 
-    rms_values_cols = [(val ** 0.5) for val in rms_values_cols]
+    for rank, score in enumerate(performance_score_copy):
+        score_to_rank[score] = rank + 1
 
-    ########################### Normalize the test matrix #############
+    ranks = [score_to_rank[r] for r in performance_score]
 
+    return ranks
+
+def get_performance_score(euclidean_dist_best, euclidean_dist_worst, n_rows):
+    performance_score = [0 for _ in range(n_rows)]
 
     for row in range(n_rows):
-        
-        for col in range(n_cols):
-            ## use the rms_values to normalize matrix
-            test_matrix1[row][col] /= rms_values_cols[col]
 
-            # multiply the matrix with weight of each col
-            test_matrix1[row][col] *= weights[col]
+        performance_score[row] = (euclidean_dist_worst[row] / (euclidean_dist_best[row] + euclidean_dist_worst[row]))
+
+    return performance_score
 
 
-
-    ############ find the ideal best value for each column
-    ideal_best_values_for_cols = [0 for _ in range(n_cols)]
-
-    ideal_worst_values_for_cols = [0 for _ in range(n_cols)]
-
-    def get_best_val_func(action):
-
-        return max if (action == '+') else min
-
-    def get_worst_val_func(action):
-
-        return max if (action == '-') else min
-
-
-    for col in range(n_cols):
-
-        # get the function producting best result
-        best_val_func = get_best_val_func(actions[col])
-
-        worst_val_func = get_worst_val_func(actions[col])
-
-        # apply the function to jth column
-        jth_col = [test_matrix1[row][col] for row in range(n_rows)]
-
-        result_best = best_val_func(jth_col)
-
-        result_worst = worst_val_func(jth_col)
-
-        # update_value_of ideal_values_for_cols[jth_col] = result
-        ideal_best_values_for_cols[col] = result_best
-
-        ideal_worst_values_for_cols[col] = result_worst
-
-
-    ####### Find the ideal worst value for each column
-
-    # ideal_worst_values_for_cols = [0 for _ in range(n_cols)]
-
-    # def get_worst_val_func(action):
-
-    #     return max if (action == '-') else min
-
-
-    # for col in range(n_cols):
-
-    #     # get the function producting best result
-    #     worst_val_func = get_worst_val_func(actions[col])
-
-    #     # apply the function to jth column
-    #     jth_col = [test_matrix1[row][col] for row in range(n_rows)]
-
-    #     result = worst_val_func(jth_col)
-
-    #     # update_value_of ideal_values_for_cols[jth_col] = result
-    #     ideal_worst_values_for_cols[col] = result
-
-
-    ## Find euclidean dist of each row from the ideal best value
-
+def get_euclidean_dists(test_matrix1, ideal_best_values_for_cols, ideal_worst_values_for_cols, n_rows, n_cols):
     euclidean_dist_best = [0 for _ in range(n_rows)]
 
     euclidean_dist_worst = [0 for _ in range(n_rows)]
@@ -116,34 +53,118 @@ def main():
 
     euclidean_dist_worst = [(val ** 0.5) for val in euclidean_dist_worst]
 
+    return euclidean_dist_best, euclidean_dist_worst
 
-    ########### Calculate performance score #########################
 
-    performance_score = [0 for _ in range(n_rows)]
 
+def get_ideal_values(test_matrix1, actions, n_rows, n_cols):
+    ideal_best_values_for_cols = [0 for _ in range(n_cols)]
+
+    ideal_worst_values_for_cols = [0 for _ in range(n_cols)]
+
+    def get_best_val_func(action):
+
+        return max if (action == '+') else min
+
+    def get_worst_val_func(action):
+
+        return max if (action == '-') else min
+
+
+    for col in range(n_cols):
+
+        jth_col = [test_matrix1[row][col] for row in range(n_rows)]
+
+
+        # get the ideal best result of the column
+        best_val_func = get_best_val_func(actions[col])
+
+        result_best = best_val_func(jth_col)
+
+        ideal_best_values_for_cols[col] = result_best
+
+
+        # get the ideal worst result of the column
+        worst_val_func = get_worst_val_func(actions[col])
+
+        result_worst = worst_val_func(jth_col)
+
+        ideal_worst_values_for_cols[col] = result_worst
+
+    
+    return ideal_best_values_for_cols, ideal_worst_values_for_cols
+
+
+def normalize_matrix(test_matrix1, n_rows, n_cols, rms_values_cols, weights):
     for row in range(n_rows):
+        
+        for col in range(n_cols):
+            ## use the rms_values to normalize matrix
+            test_matrix1[row][col] /= rms_values_cols[col]
 
-        performance_score[row] = (euclidean_dist_worst[row] / (euclidean_dist_best[row] + euclidean_dist_worst[row]))
+            # multiply the matrix with weight of each col
+            test_matrix1[row][col] *= weights[col]
 
+
+def get_rms_for_cols(test_matrix1, n_rows, n_cols):
+    
+    rms_values_cols = [0 for _ in range(n_cols)]
+    
+    for col in range(n_cols):
+    
+        for row in range(n_rows):
+    
+            rms_values_cols[col] += (test_matrix1[row][col] ** 2)
+    
+    rms_values_cols = [(val ** 0.5) for val in rms_values_cols]
+    
+    return rms_values_cols
+
+def apply_TOPSIS(test_matrix1, weights, actions):
+    ## calculate n_rows and n_cols here #######
+    n_rows = len(test_matrix1)
+
+    n_cols = len(test_matrix1[0])
+
+    rms_values_cols = get_rms_for_cols(test_matrix1, n_rows, n_cols)
+
+    ########################### Normalize the test matrix #############
+    normalize_matrix(test_matrix1, n_rows, n_cols, rms_values_cols, weights)
+    
+    # get ideal best and worst values for all columns
+    ideal_best_values_for_cols, ideal_worst_values_for_cols = get_ideal_values(test_matrix1, actions, n_rows, n_cols)
+
+    # get euclidean distances of each row from ideal best cases and ideal worst values
+    euclidean_dist_best, euclidean_dist_worst = get_euclidean_dists(test_matrix1, ideal_best_values_for_cols, ideal_worst_values_for_cols, n_rows, n_cols)
+
+
+    performance_score = get_performance_score(euclidean_dist_best, euclidean_dist_worst, n_rows)
 
     ########## Find ranks ###################################3
-    performance_score_copy = performance_score.copy()
+    ranks = get_ranks(performance_score)
 
-    performance_score_copy.sort(reverse=True)
+    merge_matrix_and_scores(test_matrix1, n_rows, performance_score, ranks)
 
-    score_to_rank = {}
 
-    for rank, score in enumerate(performance_score_copy):
-        score_to_rank[score] = rank + 1
+def main():
+    test_matrix1 = [
+    [250, 16, 12, 5],
+    [200, 16, 8, 3],
+    [300, 32, 16, 4],
+    [275, 32, 8, 4],
+    [225, 16, 16, 2]
+    ]
 
-    ranks = [score_to_rank[r] for r in performance_score]
+    weights = [0.25, 0.25, 0.25, 0.25]
+    actions = ['-', '+', '+', '+']
 
-    for row in range(n_rows):
-
-        test_matrix1[row] = test_matrix1[row] + [performance_score[row], ranks[row]]
+    apply_TOPSIS(test_matrix1, weights, actions)
 
     for row in test_matrix1:
         print(row)
+
+
+
 
 
 if __name__ == "__main__":
